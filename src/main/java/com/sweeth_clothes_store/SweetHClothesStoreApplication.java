3a0cd482d70028680f7c680086845ac4df5d12e8
Tran Van Nhan <ads.tranvannhan@gmail.com>
2024-05-27 15:18:51 +0700

    Init Project

---
+++ src/main/java/com/sweeth_clothes_store/SweetHClothesStoreApplication.java
@@ -0,0 +1,13 @@
+package com.sweeth_clothes_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetHClothesStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetHClothesStoreApplication.class, args);
+	}
+
+}
