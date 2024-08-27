c9bff642641822bc8a785309cf4adbe6942a1003
ducthuan1510 <lyducthuan890@gmail.com>
2024-08-27 10:19:41 +0700

    update quantity ctsp

--- src/main/resources/templates/customer/js/singleProduct.js
+++ src/main/resources/templates/customer/js/singleProduct.js
@@ -1,31 +1,57 @@
 var app = angular.module('singleProductApp', []);
 
-app.controller('SingleProductController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+app.controller('SingleProductController', ['$scope', '$http', '$location', function($scope, $http, $location) {
     $scope.cart = [];
-    $scope.showSuccessAlert = false;
+    $scope.quantity = 1; // Số lượng mặc định
+    $scope.showSuccessMessage = false;
 
-    $scope.addToCart = function (product) {
+    $scope.addToCart = function(product) {
         let existingProductIndex = $scope.cart.findIndex(item => item.id === product.id);
 
         if (existingProductIndex !== -1) {
-            $scope.cart[existingProductIndex].quantity++;
+            $scope.cart[existingProductIndex].quantity += $scope.quantity;
         } else {
-            product.quantity = 1;
+            product.quantity = $scope.quantity;
             $scope.cart.push(product);
         }
 
         localStorage.setItem('cart', JSON.stringify($scope.cart));
-        $('#successModal').modal('show');
+
+        // Hiển thị toast thông báo thành công
+        $scope.showSuccessMessage = true;
+
+        // Ẩn toast sau 3 giây
+        setTimeout(function() {
+            $scope.$apply(function() {
+                $scope.showSuccessMessage = false;
+            });
+        }, 3000); // Thay đổi thời gian nếu cần
     };
 
-    $scope.increaseQuantity = function (item) {
-        let index = $scope.cart.findIndex(product => product.id === item.id);
+    $scope.increaseQuantity = function(product) {
+        $scope.quantity++;
+        saveQuantity(); // Lưu số lượng vào localStorage
+    };
 
-        if (index !== -1) {
-            $scope.cart[index].quantity++;
-            localStorage.setItem('cart', JSON.stringify($scope.cart));
+    $scope.decreaseQuantity = function(product) {
+        if ($scope.quantity > 1) {
+            $scope.quantity--;
+            saveQuantity(); // Lưu số lượng vào localStorage
         }
     };
+
+    function saveQuantity() {
+        localStorage.setItem('productQuantity', $scope.quantity);
+    }
+
+    function loadQuantity() {
+        var savedQuantity = localStorage.getItem('productQuantity');
+        if (savedQuantity) {
+            $scope.quantity = parseInt(savedQuantity, 10);
+        }
+    }
+
+    loadQuantity();
 
     var storedCart = localStorage.getItem('cart');
     if (storedCart) {
@@ -37,10 +63,13 @@ app.controller('SingleProductController', ['$scope', '$http', '$location', funct
 
     function getQueryParams() {
         var params = {};
-        var parts = $location.absUrl().split('?')[1].split('&');
-        for (var i = 0; i < parts.length; i++) {
-            var param = parts[i].split('=');
-            params[param[0]] = param[1];
+        var queryString = $location.absUrl().split('?')[1];
+        if (queryString) {
+            var parts = queryString.split('&');
+            for (var i = 0; i < parts.length; i++) {
+                var param = parts[i].split('=');
+                params[param[0]] = decodeURIComponent(param[1]);
+            }
         }
         return params;
     }
@@ -49,13 +78,13 @@ app.controller('SingleProductController', ['$scope', '$http', '$location', funct
     var productId = params['id'];
 
     $http.get('http://localhost:8080/api/v1/product/public/' + productId)
-        .then(function (response) {
+        .then(function(response) {
             $scope.product = response.data;
 
             // Ảnh chính mặc định
             $scope.mainImage = $scope.product.thumbnailImage;
         })
-        .catch(function (error) {
+        .catch(function(error) {
             console.error('Error fetching product data:', error);
         });
 
